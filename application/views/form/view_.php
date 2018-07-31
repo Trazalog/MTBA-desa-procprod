c9b2fc4bf022c735b9d3559da19964cd565af05b
Unknown <fleiva@trazalog.com>
2018-07-31 12:03:53 -0300

    Patalla Formularios

--- application/views/form/view_.php
+++ application/views/form/view_.php
@@ -2,10 +2,13 @@
 
 <section class="content">
   <div class="row">
-    <div class="col-xs-12">
+    <div class="col-md-12">
       <div class="box">
         <div class="box-header">
-          <h2 class="box-title ">Registro de Piezas No Mecanizadas</h2>
+        <Center>
+          <h2 class="title "><?php echo $list[0]['nombre']; ?></h2>
+          </Center>
+          
            <?php
           // if (strpos($permission,'Add') !== false) {
           //   echo '<button class="btn btn-block btn-success" style="width: 100px; margin-top: 10px;" id="listado">Ver Listado</button>';
@@ -13,7 +16,7 @@
           ?>
         </div><!-- /.box-header -->
         <div class="box-body">            
-          <form class="form-horizontal" style="padding:0px 15px;" role="form" action="<?php base_url();?>Form/guardar" method="POST" >
+          <form class="form-horizontal" style="padding:20px 20px;" role="form" action="<?php base_url();?>Form/guardar" method="POST" >
             <div class="row" >
 
               <div class="col-sm-12 col-md-12">
@@ -22,79 +25,89 @@
                 <br>
 
                 <?php        
-                echo "<table id='tabla' class='table table-bordered table-hover'>"; 
+                echo "<table id='tabla' class='table table-hover'>";                 
                   
                   echo "<tbody>";
 
                     $categ = "";
                     $grup = "";
                     foreach($list as $a){
-                    
+                      
                       // Muestra categoria
-                      echo "<tr>"; 
-                        echo "<td colspan ='2'>";  
+                     
                     
                           $regCat = $a['nomCategoria'];
                           if ($categ != $regCat) {
-                            
-                            echo "<h4>" .$a['nomCategoria']. "</h4>";
+                            echo "<tr>"; 
+                            echo "<td colspan ='2'>";
+
+                            echo "<h3 align='center'>".$a['nomCategoria']."</h3>";
+                            echo "</td>";
+                            echo "</tr>";  
                             // echo "<input type='hidden' class='nomCategoria' id='categoria' name='' value='".$a['nomCategoria']."'>";
-                            echo "<br>";
+                            //echo "<br>";
                             $categ = $a['nomCategoria'];
                           }                        
-                        echo "</td>";
-                      echo "</tr>";                    
+                    
+                        
+                      if(strlen($a["nomGrupo"])>0){
 
                       // Muestra grupo
-                      echo "<tr>"; 
-                        echo "<td colspan ='2'>";    
+                        
                           
                           $regGrupo = $a["nomGrupo"];
                           if(strcmp($grup, $regGrupo) == 0){  //cadenas iguales
-
                             $grup = $a["nomGrupo"];
-                            echo "<br>";
-                          }else{                             //cadenas distintas
+                            
+                          }
+                          
+                          else{                             //cadenas distintas
                             //echo "entre por if";
-                            echo $a["nomGrupo"];
+                            echo "<tr>"; 
+                            echo "<td colspan ='2'>";  
+                            echo "<h4>".$a["nomGrupo"]."</h4>";
+                            echo "</td>";
+                            echo "</tr>";
                             //echo "<input type='hidden' class='nomGrupo' id='grupo' name='".$a["nomGrupo"]."' value='".$a["nomGrupo"]."'>";
-                            echo "<br>";
+                            
                             $grup = $a["nomGrupo"];
                             //echo "<br>";
                           }
-                        echo "</td>";
-                      echo "</tr>";                      
-
+                                              
+                      }
+                     
                       // Muestra el nombre del dato  
                       echo "<tr>";
-                        echo "<td>" ;                      
-                          //echo $a["nomValor"];
-                          $etiqueta = $a["nomTipoDatos"]; 
-                          echo "<label for='".$a['idValor']."' > ".$a["nomValor"]."</label>";
+                        echo "<td>" ;              
+                         
+                        $etiqueta = $a["nomTipoDatos"]; 
+                      //echo $a["nomValor"];
 
-                        echo "</td>";  
+                        echo "<h4 ' style='margin-left: 60px'; for='".$a['idValor']."' > ".$a["nomValor"]."</h4>";
+
+                        echo "</td>"; 
                         echo "<td>";  
-
+                                         
                         // muestra el componente a llenar o el select  
                           switch ($etiqueta) {
                                 case "select":
-                                    echo "<select class='form-control sel' name='".$a['idValor']."' id='".$a['idValor']."' style='width: 30%';>
-                                      <option value= '-1'>Seleccionar...</option>
-                                    </select>";
-                                    echo "";
+                                echo "<select class='form-control sel' name='".$a['idValor']."' id='".$a['idValor']."' style='width: 100%';>
+                                  <option value= '-1'>Selecciona...</option>
+                                </select>";
                                     break;
                                 case "input":
-                                    echo "<input type='text' class='form-control inp' name='".$a['idValor']."' id='".$a['idValor']."' style='width: 30%';'>";
+                                    echo "<input type='text' class='form-control inp' name='".$a['idValor']."' id='".$a['idValor']."' style='width: 100%';'>";
                                     break;
                                 case "checkbox":                                    
-                                    echo "<input type='checkbox' class='check' value='' name='".$a['idValor']."'";
+                                    echo "<input type='checkbox' style='transform: scale(1.4);' value='' name='".$a['idValor']."';'>";
                                     break; 
                                 case "textarea":
-                                    echo "<textarea class='form-control' name='".$a['idValor']."' id='".$a['idValor']."' rows='3'></textarea>";
+                                    echo "<textarea class='form-control' name='".$a['idValor']."' id='".$a['idValor']."' rows='2'></textarea>";
                                     break;                                 
-                          }                          
-                        echo "</td>";  
-                      echo "</tr>"; 
+                          }
+                          echo "</td>";
+                          echo "</tr>"; 
+                          
                     }
 
                   echo "</tbody>";
@@ -109,12 +122,14 @@
 
 
             <div class="modal-footer">
+
+            <input type="button" id="btn" class="btn"
+            value="IMPRIMIR" onclick="javascript:print()";"toogle()">
+
             <!-- <button type="button" id=""class="btn btn-success" onclick="">Aprobar</button>
             <button type="button" class="btn btn-danger" onclick="">Rechazar</button> -->
-            <button type="submit" class="btn btn-success" >Guardar</button>
+            <!-- <button type="submit" class="btn btn-success" >Guardar</button>
           </div>  
-
-
 
           </form>  
           
@@ -128,6 +143,12 @@
 
 
 <script>
+
+//$(".btn").click(function() {
+//  $(this).fadeOut(fast);
+  //$(this).fadeIn(2000);
+//});
+
 
 $(document).ready(function(event) {    
     
@@ -150,6 +171,7 @@ $(document).ready(function(event) {
               },
           dataType: 'json'
   });
+
 
   function llenaComp(data){
    
